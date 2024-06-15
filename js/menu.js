bf5053d8c3164f3ba04fd439864a3f923906b6c0
andresarizalputra <andre.sarizal.putra@gmail.com>
2024-06-15 22:21:18 +0700

    commits 16

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ var menuList = menu.find('ul:first');
 var listItems = menu.find('li').not('#responsive-tab');
 
 // Create responsive trigger
-menuList.prepend('<li id="responsive-tab"><a href="#">Menu</a></li>');
+menuList.prepend('<li id="responsive-tab"><a>Menu</a></li>');
 
 // Toggle menu visibility
 menu.on('click', '#responsive-tab', function(){
@@ -15,3 +15,20 @@ menu.on('click', '#responsive-tab', function(){
 });
 });
 } )( jQuery );
+
+$(document).ready(function () {  
+	var top = $('#cssmenu').offset().top - parseFloat($('#cssmenu').css('marginTop').replace(/auto/, 100));
+	$(window).scroll(function (event) {
+	  // what the y position of the scroll is
+	  var y = $(this).scrollTop();
+  
+	  // whether that's below the form
+	  if (y >= top) {
+		// if so, ad the fixed class
+		$('#cssmenu').addClass('fixed');
+	  } else {
+		// otherwise remove it
+		$('#cssmenu').removeClass('fixed');
+	  }
+	});
+  });
